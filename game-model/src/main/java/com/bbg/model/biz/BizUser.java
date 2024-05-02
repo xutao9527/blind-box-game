8b16faa622ba65caba1d6b94fe6a707932af01b7
xutao <xtxt9527@gmail.com>
2024-05-02 11:11:13 +0700

    脱敏

--- game-model/src/main/java/com/bbg/model/biz/BizUser.java
+++ game-model/src/main/java/com/bbg/model/biz/BizUser.java
@@ -2,6 +2,8 @@ package com.bbg.model.biz;
 
 import com.bbg.model.record.BizUserRecord;
 
+import com.mybatisflex.annotation.ColumnMask;
+import com.mybatisflex.core.mask.Masks;
 import lombok.*;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.experimental.SuperBuilder;
@@ -11,4 +13,17 @@ import lombok.experimental.SuperBuilder;
 @Schema(description = "业务用户")
 public class BizUser extends BizUserRecord {
 
+    /**
+     * 手机号
+     */
+    @Schema(description = "手机号")
+    @ColumnMask(Masks.MOBILE)
+    private String mobile;
+
+    /**
+     * 密码
+     */
+    @Schema(description = "密码")
+    @ColumnMask(Masks.PASSWORD)
+    private String password;
 }
--- game-model/src/main/java/com/bbg/model/sys/SysUser.java
+++ game-model/src/main/java/com/bbg/model/sys/SysUser.java
@@ -2,6 +2,8 @@ package com.bbg.model.sys;
 
 import com.bbg.model.record.SysUserRecord;
 
+import com.mybatisflex.annotation.ColumnMask;
+import com.mybatisflex.core.mask.Masks;
 import lombok.*;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.experimental.SuperBuilder;
@@ -10,5 +12,10 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @Schema(description = "系统用户")
 public class SysUser extends SysUserRecord {
-
+    /**
+     * 密码
+     */
+    @Schema(description = "密码")
+    @ColumnMask(Masks.PASSWORD)
+    private String password;
 }
