cc491fcc30d38dfbd5af5988a91764761d115f04
xutao <xtxt9527@gmail.com>
2024-05-03 09:10:46 +0700

    公平性验证

--- game-common/game-common-core/src/main/java/com/bbg/core/aop/RedisLockAspect.java
+++ game-common/game-common-core/src/main/java/com/bbg/core/aop/RedisLockAspect.java
@@ -7,12 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.redisson.api.RFuture;
 import org.redisson.api.RLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.TimeUnit;
+
 
 @Aspect
 @Component
