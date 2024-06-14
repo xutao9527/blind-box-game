c7626e05a20871e5cc170c06e2f0b33740c5b640
xutao <xtxt9527@gmail.com>
2024-06-14 14:09:22 +0700

    第三方

--- game-third/src/main/java/com/bbg/third/ThirdServiceApplication.java
+++ game-third/src/main/java/com/bbg/third/ThirdServiceApplication.java
@@ -1,9 +1,17 @@
 package com.bbg.third;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@ImportAutoConfiguration
+@MapperScan("com.bbg.core.mapper")
+@ComponentScan(basePackages = {"com.bbg.core", "com.bbg.third"})
+@EnableTransactionManagement
 public class ThirdServiceApplication {
 
 	public static void main(String[] args) {
