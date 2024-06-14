e26a2989109b4b505a70467dd4e9e8ab482355ce
xutao <xtxt9527@gmail.com>
2024-06-14 11:40:58 +0700

    第三方

---
+++ game-third/src/main/java/com/bbg/gamethird/GameThirdApplication.java
@@ -0,0 +1,13 @@
+package com.bbg.gamethird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameThirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameThirdApplication.class, args);
+	}
+
+}
---
+++ game-third/src/test/java/com/bbg/gamethird/GameThirdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbg.gamethird;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameThirdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
