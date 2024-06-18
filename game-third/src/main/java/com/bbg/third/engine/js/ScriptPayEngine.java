d183905b9c48887bb9d5cb1a5e1a50f9d56591d4
xutao <xtxt9527@gmail.com>
2024-06-18 20:31:14 +0700

    脚本编辑器

--- game-third/src/main/java/com/bbg/third/engine/js/ScriptPayEngine.java
+++ game-third/src/main/java/com/bbg/third/engine/js/ScriptPayEngine.java
@@ -3,6 +3,8 @@ package com.bbg.third.engine.js;
 import com.bbg.model.biz.BizPayPlatform;
 import com.bbg.model.biz.BizUser;
 import com.bbg.third.engine.PayEngine;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -12,6 +14,15 @@ public class ScriptPayEngine implements PayEngine {
 
     @Override
     public Object execCall(BizUser bizUser, BizPayPlatform bizPayPlatform, BigDecimal money) {
+        try(Context context = Context.create()) {
+            context.getBindings("js").putMember("bizUser", bizUser);
+            context.getBindings("js").putMember("bizPayPlatform", bizPayPlatform);
+            context.getBindings("js").putMember("money", money);
+            Value value = context.eval("js", bizPayPlatform.getCallEngine());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
