553d04c03b95d0c810a62ff551a6f7491dd3fe31
xutao <xtxt9527@gmail.com>
2024-06-19 10:52:39 +0700

    国际化

--- game-admin-web/src/main.js
+++ game-admin-web/src/main.js
@@ -7,8 +7,13 @@ import "@/core/common/browser_patch.js"
 import {bbgConf} from "@/config/index.js";
 import router from '@/router'
 import store from "@/store/index";
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import ElementPlus from 'element-plus'
 
 const app = createApp(App)
+app.use(ElementPlus, {
+    locale: zhCn,
+})
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
     bbgConf.iconList.push(key);
