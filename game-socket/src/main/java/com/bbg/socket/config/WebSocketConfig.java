3785ddceccd3e2e714eb6e96ed75a6807d3c3729
xutao <xtxt9527@gmail.com>
2024-06-21 15:41:47 +0700

    socket

---
+++ game-socket/src/main/java/com/bbg/socket/config/WebSocketConfig.java
@@ -0,0 +1,34 @@
+package com.bbg.socket.config;
+
+import com.bbg.socket.handler.GameWebSocketHandler;
+import org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableWebFlux
+@Configuration
+public class WebSocketConfig  {
+
+    @Bean
+    public HandlerMapping handlerMapping(){
+        Map<String, WebSocketHandler> map = new HashMap<>();
+        map.put("/echo", new GameWebSocketHandler());
+        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+        mapping.setUrlMap(map);
+        mapping.setOrder(-1);
+        return mapping;
+    }
+
+    @Bean
+    public WebSocketHandlerAdapter handlerAdapter() {
+        return new WebSocketHandlerAdapter();
+    }
+}
---
+++ game-socket/src/main/java/com/bbg/socket/handler/GameWebSocketHandler.java
@@ -0,0 +1,16 @@
+package com.bbg.socket.handler;
+
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Mono;
+
+public class GameWebSocketHandler implements WebSocketHandler {
+    @Override
+    public Mono<Void> handle(WebSocketSession session) {
+        return session.send(
+                session.receive()
+                        .map(msg -> "服务端返回：" + msg.getPayloadAsText())
+                        .map(session::textMessage)
+        );
+    }
+}
