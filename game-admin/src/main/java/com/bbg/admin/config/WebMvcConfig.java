120c986e3d581540dba025c55e0a52733d3eb71e
xutao <xtxt9527@gmail.com>
2024-06-21 21:59:09 +0700

    文档

--- game-admin/src/main/java/com/bbg/admin/config/WebMvcConfig.java
+++ game-admin/src/main/java/com/bbg/admin/config/WebMvcConfig.java
@@ -15,7 +15,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(sessionInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/doc")                                // 在线接口文档
-                .excludePathPatterns("/api-docs")                           // 在线接口文档
+                .excludePathPatterns("/v3/api-docs")                           // 在线接口文档
                 .excludePathPatterns("/swagger-ui/**")                      // 在线接口文档
                 .excludePathPatterns("/api-docs/swagger-config")            // 在线接口文档
                 .excludePathPatterns("/sysUser/login");
