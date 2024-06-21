927568e382064c8c08378945bc222bdf844b6d09
xutao <xtxt9527@gmail.com>
2024-06-21 16:20:56 +0700

    socket

--- game-socket/src/main/java/com/bbg/socket/config/WebSocketConfig.java
+++ game-socket/src/main/java/com/bbg/socket/config/WebSocketConfig.java
@@ -8,7 +8,10 @@ import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.server.WebSocketService;
+import org.springframework.web.reactive.socket.server.support.HandshakeWebSocketService;
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+import org.springframework.web.reactive.socket.server.upgrade.ReactorNettyRequestUpgradeStrategy;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,7 +33,12 @@ public class WebSocketConfig  {
 
     @Bean
     public WebSocketHandlerAdapter handlerAdapter() {
-        return new WebSocketHandlerAdapter();
+        return new WebSocketHandlerAdapter(webSocketService());
+    }
+
+    @Bean
+    public WebSocketService webSocketService() {
+        return new HandshakeWebSocketService(new ReactorNettyRequestUpgradeStrategy());
     }
 
 }
