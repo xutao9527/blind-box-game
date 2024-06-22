d01572e539ecdc75b7696562c7c2b921c1925a6f
xutao <xtxt9527@gmail.com>
2024-06-22 10:33:05 +0700

    websocket

--- game-socket/src/main/java/com/bbg/socket/controller/WebSocketController.java
+++ game-socket/src/main/java/com/bbg/socket/controller/WebSocketController.java
@@ -6,6 +6,7 @@ import com.bbg.socket.entity.WebSocketSender;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.validation.annotation.Validated;
@@ -13,22 +14,27 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.ConcurrentMap;
 
-
+@Slf4j
+@Validated
 @RestController
 @Tag(name = "WebSocket接口")
 @RequiredArgsConstructor
-@Validated
 public class WebSocketController {
 
-    private static final Logger log = LoggerFactory.getLogger(WebSocketController.class);
     public final ConcurrentMap<String, WebSocketSender> adminSenderMap;
+    public final ConcurrentMap<String, WebSocketSender> gameSenderMap;
 
-    @PostMapping(value = "/send")
-    @Operation(summary = "管理后台推送消息", description = "管理后台推送消息")
-    public ApiRet<Boolean> sendMessage(@RequestBody WebSocketMsg webSocketMsg) {
-        log.info("send message: {}", webSocketMsg.getMessage());
+    @PostMapping(value = "admin/send")
+    @Operation(summary = "后台推送消息", description = "管理后台推送消息")
+    public ApiRet<Boolean> sendAdminMessage(@RequestBody WebSocketMsg webSocketMsg) {
         adminSenderMap.forEach((id, sender) -> sender.send(webSocketMsg.getMessage()));
         return ApiRet.buildOk();
     }
 
+    @PostMapping(value = "game/send")
+    @Operation(summary = "游戏推送消息", description = "游戏推送消息")
+    public ApiRet<Boolean> sendGameMessage(@RequestBody WebSocketMsg webSocketMsg) {
+        gameSenderMap.forEach((id, sender) -> sender.send(webSocketMsg.getMessage()));
+        return ApiRet.buildOk();
+    }
 }
