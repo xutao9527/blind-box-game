08548eaf50d2af4c74d48587936f80edfa1e64d3
xutao <xtxt9527@gmail.com>
2024-06-25 17:46:34 +0700

    租户

--- game-admin-web/src/core/tenant/index.js
+++ game-admin-web/src/core/tenant/index.js
@@ -1,6 +1,4 @@
 import {http} from "@/core/axios/index.js";
-import {bbgConf} from "@/config/index.js";
-
 
 export class TenantObject{
     async constructor() {
@@ -8,6 +6,7 @@ export class TenantObject{
     }
     async fetchData() {
         this.data = await http.get("sysTenant/getTenantList")
+        console.log("TenantObject",this.data)
     }
 }
 
