8e5f4e427fac3a8cd8953527e5302ff157b2c1cc
xutao <xtxt9527@gmail.com>
2024-06-28 17:35:01 +0700

    租户

--- game-admin-web/src/components/permission/index.js
+++ game-admin-web/src/components/permission/index.js
@@ -3,11 +3,10 @@ export const hasPermission = {
         //自定义指令v-has：
         Vue.directive('has', {
             mounted(el, binding, vnode) {
-                console.log(el)
                 console.log(binding)
-                console.log(vnode)
             },
         });
+
     }
 };
 export default hasPermission;
