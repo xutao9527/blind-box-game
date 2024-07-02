c6e4580840715d57b1b63a26b61fc552a4d2ecce
xutao <xtxt9527@gmail.com>
2024-07-02 11:34:41 +0700

    租户

---
+++ game-box-app/src/main/java/com/bbg/box/tenant/TenantIdFactory.java
@@ -0,0 +1,26 @@
+package com.bbg.box.tenant;
+
+import com.bbg.model.sys.SysTenant;
+import com.mybatisflex.core.tenant.TenantFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+
+@Component
+public class TenantIdFactory implements TenantFactory {
+    @Override
+    public Object[] getTenantIds() {
+        RequestAttributes attributes = RequestContextHolder.getRequestAttributes();
+        if (attributes != null) {
+            Object tenantObject = attributes.getAttribute("tenantObject", RequestAttributes.SCOPE_REQUEST);
+            if (tenantObject instanceof SysTenant sysTenant) {
+                if (sysTenant.getParentId() == null) {              // 超级租户,返回空数据
+                    return new Object[]{};
+                } else {
+                    return new Object[]{sysTenant.getId()};
+                }
+            }
+        }
+        return new Object[]{};                                      // 未登录用户,返回空数据
+    }
+}
