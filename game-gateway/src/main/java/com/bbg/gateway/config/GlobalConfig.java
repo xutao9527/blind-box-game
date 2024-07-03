808dba64b73081a08fab610a9cbd94f2f7cf598d
xutao <xtxt9527@gmail.com>
2024-07-04 00:40:42 +0700

    租户

--- game-gateway/src/main/java/com/bbg/gateway/config/GlobalConfig.java
+++ game-gateway/src/main/java/com/bbg/gateway/config/GlobalConfig.java
@@ -7,9 +7,10 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class GlobalConfig {
+
     @Bean
-    @LoadBalanced     // 如果不添加，无法通过服务名进行调用，只能通过ip调用
-    public WebClient.Builder loadBalancedWebClientBuilder(){
+    @LoadBalanced
+    public WebClient.Builder loadBalanced() {
         return WebClient.builder();
     }
 }
--- game-gateway/src/main/java/com/bbg/gateway/sched/RouterChecker.java
+++ game-gateway/src/main/java/com/bbg/gateway/sched/RouterChecker.java
@@ -1,6 +1,8 @@
 package com.bbg.gateway.sched;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -8,25 +10,27 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Component
-@RequiredArgsConstructor
-public class RouterChecker {
 
-    private final RouteDefinitionLocator routeDefinitionLocator;
-    private final WebClient.Builder webBuilder;
+public class RouterChecker {
+    @Autowired
+    private  RouteDefinitionLocator routeDefinitionLocator;
+    @Autowired
+    @LoadBalanced
+    private  WebClient.Builder loadBalanced;
 
     @Scheduled(fixedRate = 5000) // 每隔 1 秒检查一次配置
     public void checkRouter() {
-        System.out.println("webBuilder = " + webBuilder);
 
-        Mono<Object> objectMono = webBuilder
-                .baseUrl("admin-server").build().get()
-                .uri("/sysTenant/all")
-                .exchangeToMono(clientResponse -> {
-                    System.out.println("clientResponse = " + clientResponse);
-                    return clientResponse.bodyToMono(Object.class);
-                });
-        Object block = objectMono.subscribe();
-        System.out.println(block);
+        String url = "lb://admin-server/sysTenant/all";
+        WebClient webClient = loadBalanced.build();
+
+        Mono<Object> objectMono = webClient
+                .get()
+                .uri(url)
+                .exchangeToMono(clientResponse -> clientResponse.bodyToMono(Object.class));
+        objectMono.subscribe();
+
+
         // List<SysTenant> sysTenantList = tenantSearchService.all();
         // System.out.println("sysTenantList = " + sysTenantList);
         // routeDefinitionLocator.getRouteDefinitions().subscribe(routeDefinition -> {
