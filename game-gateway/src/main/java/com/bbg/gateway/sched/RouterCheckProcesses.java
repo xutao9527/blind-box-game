e97707d0533a4e81595ad7b03dbab60a033faeb8
xutao <xtxt9527@gmail.com>
2024-07-04 11:52:40 +0700

    租户

--- game-gateway/src/main/java/com/bbg/gateway/sched/RouterCheckProcesses.java
+++ game-gateway/src/main/java/com/bbg/gateway/sched/RouterCheckProcesses.java
@@ -4,7 +4,6 @@ import com.bbg.model.sys.SysTenant;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.FilterDefinition;
 import org.springframework.cloud.gateway.filter.factory.StripPrefixGatewayFilterFactory;
 import org.springframework.cloud.gateway.handler.predicate.PredicateDefinition;
